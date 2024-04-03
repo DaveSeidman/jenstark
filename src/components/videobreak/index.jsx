49a8a6642e50cb23e5ed86e30526730ccc060faf
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2024-04-02 19:03:37 -0600

    Update video path for videobreak

--- src/components/videobreak/index.jsx
+++ src/components/videobreak/index.jsx
@@ -12,7 +12,7 @@ function VideoBreak() {
         loop
         playsInline
         autoPlay
-        src={`${location.pathname}/VideoBreak.mp4`} />
+        src={`./videos/VideoBreak.mp4`} />
     </div>
   )
 }
